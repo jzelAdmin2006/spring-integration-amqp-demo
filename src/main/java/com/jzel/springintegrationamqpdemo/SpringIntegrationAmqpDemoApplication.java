aff8e312ed77c022e3b923b3582e5cb087f24cd3
Johannes Zeller <jo.zeller@protonmail.com>
2024-12-01 22:26:18 +0100

    initialise project

---
+++ src/main/java/com/jzel/springintegrationamqpdemo/SpringIntegrationAmqpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jzel.springintegrationamqpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationAmqpDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringIntegrationAmqpDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jzel/springintegrationamqpdemo/SpringIntegrationAmqpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzel.springintegrationamqpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationAmqpDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
